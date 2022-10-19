9bcba4fdc67ed6a0d13ae8109c7f6e92c1abca6b
Andile-3 <andile.ntathu123@gmail.com>
2022-10-19 08:51:26 -0700

    Task 2 - x 10 Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase
-*/
+ * print_alpahbet_x10 -prints alpha 10 times
+ */
+void print_alphabet_x10 (void)
+{
+	int ten;
+	char la;
 
-void print_alphabet(void)
-{	
-	char letter;
+	for (ten = 0; ten <= 9; ten++)
+	{   
+		for (la = 'a';la <= 'z'; la++)
+			_putchar(la);
+		-putchar('\n');
+	}
+}	
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	
-	_putchar('\n');
-}

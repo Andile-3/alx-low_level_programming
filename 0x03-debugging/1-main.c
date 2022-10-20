1482a2cc657f1978fb42a57ef6ca7c3bbbb5f3f8
Andile-3 <andile.ntathu123@gmail.com>
2022-10-19 17:24:48 -0700

    Task 1 - Like, Comment, Subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinate loop
+ * Return: 0
+ */
+
+int main(void)
+{
+int t;
+
+printf("Infinate loop incomking : (\n");
+
+i = 0;
+/*
+ * while (i < 10)
+ * {       
+ *         putchar(i);
+ * }
+ */
+printf("Infinate loop avoided!  \\o/\n");
+
+return (0);
+}

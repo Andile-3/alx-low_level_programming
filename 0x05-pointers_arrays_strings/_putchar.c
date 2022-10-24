f9563ccc067fbdbe7b2ee73f01b30f4f6d3008cb
Andile-3 <andile.ntathu123@gmail.com>
2022-10-24 14:02:41 -0700

    Task 0 - 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writers character c to stdout
+ * @c: char to be printed
+ * RETURN: On success
+ */
+int _putchar(char c)
+{ 
+	return (write(i, &c, 1));
+}

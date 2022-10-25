958467e6b82e24e5a83dc612feadce884bd54e7e
Andile-3 <andile.ntathu123@gmail.com>
2022-10-25 05:27:55 -0700

    Task 0 - 98 Battery st.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int_strlen(char *s);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -12,5 +12,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest,char *src);
 int _putchar(char c);
+int _atol(char *s);
 
 #endif
